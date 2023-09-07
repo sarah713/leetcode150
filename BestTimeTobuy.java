56d93cfc59fb2548d9821cb0f353905c50531d79
Sarah <sarahkulsoom18@gmail.com>
2023-09-07 16:58:39 +0530

    Best time to buy and sell stock

---
+++ BestTimeTobuy.java
@@ -0,0 +1,10 @@
+public class BestTimeTobuy {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0, minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+}
