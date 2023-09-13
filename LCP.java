fe83ecfffedf5642468fddabcb37abadae7f990b
Sarah <sarahkulsoom18@gmail.com>
2023-09-13 22:14:03 +0530

    Longest Common Prefix

---
+++ LCP.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class LCP {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                return sb.toString();
+            }
+            sb.append(first.charAt(i));
+        }
+        return sb.toString();
+    }
+}
