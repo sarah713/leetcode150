b63051d075f6505b0541f999b5a1bf385c0a38cb
Sarah <sarahkulsoom18@gmail.com>
2023-09-14 20:15:03 +0530

    Reversed Words

---
+++ ReverseWords.java
@@ -0,0 +1,17 @@
+public class ReverseWords {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        int wordEnd = s.length();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                wordEnd = i;
+            } else if (i == 0 || s.charAt(i - 1) == ' ') {
+                if (sb.length() > 0) {
+                    sb.append(' ');
+                }
+                sb.append(s, i, wordEnd);
+            }
+        }
+        return sb.toString();
+    }
+}
